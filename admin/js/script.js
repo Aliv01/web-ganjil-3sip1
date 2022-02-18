25dac8028fa09d01e066e2d2d22dbcaf26a05ac1
Kevin Williams Permana <kevinity310@gmail.com>
2022-02-18 17:52:50 +0700

    Kevin : Rombak website

---
+++ admin/js/script.js
@@ -0,0 +1,24 @@
+const txtElemet = ['Universitas Bung Karno', 'UBK         ']
+
+let count = 0;
+let txtIndext = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik (){
+
+    if(count == txtElemet.length){
+        count = 0;
+    }
+    
+    currentTxt = txtElemet[count];
+
+    words = currentTxt.slice(0, ++txtIndext);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if (words.length == currentTxt.length){
+        count++;
+        txtIndext = 0;
+    }
+    setTimeout(ngetik, 100);
+})();
